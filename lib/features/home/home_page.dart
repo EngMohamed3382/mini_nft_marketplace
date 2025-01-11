03b2a60173292269d6a9bc026a84891bf77aa18e
Atlas Home <alhefnawimoh@hotmail.com>
2025-01-11 13:53:59 +0200

    home Screen part 5 frame

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -7,11 +7,12 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
         centerTitle: true,
-        title: const Text('NFT Marketplace',
-          style: TextStyle(fontSize: 25.23,
+        title: const Text(
+          'NFT Marketplace',
+          style: TextStyle(
+            fontSize: 25.23,
             fontFamily: FontManagers.sfProDisplayFont,
             color: Colors.white,
             fontWeight: FontWeight.bold,
@@ -19,15 +20,44 @@ class HomePage extends StatelessWidget {
         ),
         backgroundColor: Colors.transparent,
       ),
-
       body: Column(
         children: [
-          Image(image: AssetImage('asstes/images/1.jpg'))
-
-
+          Stack(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(27.03),
+                child: const Image(
+                  image: AssetImage('asstes/images/1.jpg'),
+                  height: 167.57,
+                  width: 252.26,
+                )),
+            Positioned(bottom: -8,
+                    left: 13,
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(27.03),
+                bottomRight: Radius.circular(27.03),
+                ),
+                child: Container(
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors:[ Color(0xff000000).withOpacity(0.2),
+                        Color(0xff000000),]
+                
+                    ),
+                  ),
+                    height: 54.06,
+                    width: 225.26,
+                
+                    child: Text('Art',style: TextStyle(fontSize: 19.82, fontFamily: FontManagers.sfProDisplayFont, fontWeight: FontWeight.bold),)),
+              ),
+            )
+            ]
+          ),
         ],
       ),
-
       backgroundColor: Color(0xff211134),
     );
   }
