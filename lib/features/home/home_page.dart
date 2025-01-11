3203ba46ebaa7bf7d771c9468c8cc6c33cdc3c01
Atlas Home <alhefnawimoh@hotmail.com>
2025-01-11 12:21:01 +0200

    home Screen part 2

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mini_nft_marketplace/core/resources/font_manager.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,6 +7,21 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: SafeArea(child: Container(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 20,),
+            const Text('NFT Marketplace',
+            style: TextStyle(fontSize: 25.23,
+            fontFamily: FontManagers.sfProDisplayFont,
+              color: Colors.white,
+            ),
+            ),
+          ],
+        ),
+      ) ),
       backgroundColor: Color(0xff211134),
     );
   }
