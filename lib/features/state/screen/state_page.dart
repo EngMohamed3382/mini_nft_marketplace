724fe4309c15d0673896e32d6b5107df56a7bce7
Atlas Home <alhefnawimoh@hotmail.com>
2025-01-14 22:22:32 +0200

    home Screen part 14 Add Single Scroll View

--- lib/features/state/screen/state_page.dart
+++ lib/features/state/screen/state_page.dart
@@ -12,59 +12,66 @@ class StatePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(
-          height: 27,
-        ),
-        CustomSubTitleStatePage(),
-        SizedBox(
-          height: 27,
-        ),
-        Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            CustomCategoryOfStatePage(
-              title: 'All Category',
-              iconData: Icons.menu,
-            ),
-            SizedBox(
-              width: 20,
-            ),
-            CustomCategoryOfStatePage(
-              title: 'All Chains',
-              iconData: Icons.link,
-            ),
-          ],
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 9),
-          child: ClipRRect(
-            // ignore: sort_child_properties_last
-            child: BackdropFilter(
-              filter:
-                  ImageFilter.blur(sigmaX: BlurValue.b10, sigmaY: BlurValue.b10),
-              child: Container(
-                height: 300,
-                alignment: Alignment.center,
-                //padding: EdgeInsets.all(25),
-                child: ListView.separated(itemBuilder: (context,index) => CustomTableRowStatePage(
-                    order: 1,
-                    image: AssetImageManager.topSeller2,
-                    name: 'name',
-                    subName: 'subName',
-                    count1: 'count1',
-                    percent1: 'percent1'),
-                    separatorBuilder: (context, index) => SizedBox(height: 9,), itemCount: 20),
-
-                color: ColorManager.kColorWhite.withOpacity(0.1),
-              ),
-            ),
-            borderRadius: BorderRadius.circular(40),
+    double listHeight = MediaQuery.of(context).size.height - 250;
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(
+            height: 27,
           ),
-        ),
-
-      ],
+          CustomSubTitleStatePage(),
+          SizedBox(
+            height: 27,
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              CustomCategoryOfStatePage(
+                title: 'All Category',
+                iconData: Icons.menu,
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              CustomCategoryOfStatePage(
+                title: 'All Chains',
+                iconData: Icons.link,
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 9),
+            child: ClipRRect(
+              // ignore: sort_child_properties_last
+              child: BackdropFilter(
+                filter: ImageFilter.blur(
+                    sigmaX: BlurValue.b10, sigmaY: BlurValue.b10),
+                child: Container(
+                  color: Colors.white.withOpacity(0.1),
+                  child: ListView.separated(
+                    physics: NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                      itemBuilder: (context, index) => CustomTableRowStatePage(
+                          order: 1,
+                          image: AssetImageManager.topSeller2,
+                          name: 'name',
+                          subName: 'subName',
+                          count1: 'count1',
+                          percent1: 'percent1'),
+                      separatorBuilder: (context, index) => SizedBox(
+                            height: 9,
+                          ),
+                      itemCount: 20),
+                ),
+              ),
+              borderRadius: BorderRadius.circular(40),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
