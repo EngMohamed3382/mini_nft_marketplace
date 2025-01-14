a4f613ba65f2c2c8b8edd93b5d49c794d6d054bc
Atlas Home <alhefnawimoh@hotmail.com>
2025-01-14 19:30:42 +0200

    home Screen part 12 Make Custom Table Row of Stat page

--- lib/features/state/screen/state_page.dart
+++ lib/features/state/screen/state_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mini_nft_marketplace/core/resources/asset_image_manager.dart';
 import 'package:mini_nft_marketplace/features/state/widgets/custom_category_of_state_page.dart';
 import 'package:mini_nft_marketplace/features/state/widgets/custom_sub_title.dart';
+import 'package:mini_nft_marketplace/features/state/widgets/custom_table_row_state_page.dart';
 
 class StatePage extends StatelessWidget {
   const StatePage({super.key});
@@ -33,68 +34,9 @@ class StatePage extends StatelessWidget {
             ),
           ],
         ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            children: [
-              Text('1'),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(9),
-                child: Image(
-                  fit: BoxFit.cover,
-                  image: AssetImage(AssetImageManager.topSeller1),
-                  width: 39,
-                  height: 39,
-                ),
-              ),
-              Container(
-                width: 115,
-                height: 39,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Azumi',
-                      style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      'Azumi',
-                      style: TextStyle(fontSize: 11),
-                    )
-                  ],
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.link,
-                        size: 15,
-                      ),
-                      Text(
-                        '285475.02',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 13,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  ),
-                  Text('3.99%',
-                      style: TextStyle(
-                          color: Colors.green,
-                          fontSize: 12,
-                          fontWeight: FontWeight.bold)),
-                ],
-              )
-            ],
-          ),
-        ),
+        CustomTableRowStatePage(order: 1, image: AssetImageManager.topSeller2, name: 'name', subName: 'subName', count1: 'count1', percent1: 'percent1')
+
+
       ],
     );
   }
---
+++ lib/features/state/widgets/custom_table_row_state_page.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+
+
+class CustomTableRowStatePage extends StatelessWidget {
+  const CustomTableRowStatePage({super.key, required this.order, required this.image, required this.name, required this.subName, required this.count1, required this.percent1});
+
+  final int order;
+  final String image;
+  final String name;
+  final String subName;
+  final String count1;
+  final String percent1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(9.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('$order'),
+          SizedBox(width: 10,),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(9),
+            child: Image(
+              fit: BoxFit.cover,
+              image: AssetImage(image),
+              width: 39,
+              height: 39,
+            ),
+          ),
+          SizedBox(width: 13,),
+          Container(
+            width: 115,
+            height: 39,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                    name,
+                  style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  subName,
+                  style: TextStyle(fontSize: 11),
+                )
+              ],
+            ),
+          ),
+          SizedBox(width: 28,),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Row(
+                children: [
+                  Icon(
+                    Icons.link,
+                    size: 15,
+                  ),
+                  Text(
+                    count1,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 13,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              Text(percent1,
+                  style: TextStyle(
+                      color: Colors.green,
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold)),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
